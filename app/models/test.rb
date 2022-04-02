02f80017cc1c51752d85cd02a81804cee736e7fc
ekateriniy <ekaterin.yu@gmail.com>
2022-04-02 17:21:06 +0400

    feat: add descending test order by category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.order_by_category_desc(category)
+    category_id = Category.find_by(title: category).id
+    self.joins('JOIN categories ON tests.category_id = categories.id').where(category_id: category_id).order(id: :desc)
+  end
 end
