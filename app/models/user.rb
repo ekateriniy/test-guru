eaa89850758b44bba5b2df38ada1d544335d7f76
ekateriniy <ekaterin.yu@gmail.com>
2022-04-04 08:20:44 +0400

    chore: add symbols instead of sql-query

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   def tests_on_level(level)
-    Test.joins('JOIN tests_users ON tests_users.test_id = tests.id')
-      .where('tests.level = ? AND tests_users.user_id = ?', level, id)
+    Test.where(level: level)
+      .joins('JOIN tests_users ON tests_users.test_id = tests.id')
+      .where(tests_users: { user_id: id })
   end
 end
