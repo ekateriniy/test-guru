7d7b1de4b5509fc509fe6cb5e8938e1d96a54f65
ekateriniy <ekaterin.yu@gmail.com>
2022-04-05 10:40:25 +0400

    fix: add destroy dependency between associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  
-  has_many :questions
+
+  has_many :questions, dependent: :destroy
   has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :users, through: :tests_users, dependent: :destroy
 
   def self.titles_order_by_category_desc(category)
     joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
-  has_many :tests, through: :tests_users
-  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :tests, through: :tests_users, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   def tests_on_level(level)
     Test.where(level: level)
