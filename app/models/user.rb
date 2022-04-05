67711ef9ac43f96eef82ac64b32576004a0018db
ekateriniy <ekaterin.yu@gmail.com>
2022-04-05 10:42:44 +0400

    refactor: refactor methods according to assotiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   def tests_on_level(level)
-    Test.where(level: level)
-      .joins('JOIN tests_users ON tests_users.test_id = tests.id')
-      .where(tests_users: { user_id: id })
+    tests.where(level: level)
   end
 end
