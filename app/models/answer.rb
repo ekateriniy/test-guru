0518b3e4f3202fbadc6d44d5b5e6fbb29dcfd304
ekateriniy <ekaterin.yu@gmail.com>
2022-04-07 10:12:58 +0400

    fix: validate_quantity

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_quantity, on: [:save, :create]
+  validate :validate_quantity, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_quantity
-    errors.add(:answers_quantity, 'There must be 1 to 4 answers per question') unless question.answers.size < 4
+    errors.add(:answers_quantity, 'There must be 1 to 4 answers per question') if question.answers.count >= 4
   end
 end
