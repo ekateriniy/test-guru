723e3ffcc0b007937c2e8130804aba85a9e32d68
ekateriniy <ekaterin.yu@gmail.com>
2022-04-08 08:21:50 +0400

    fix: add uniq index of tests level and title to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_100959) do
+ActiveRecord::Schema.define(version: 2022_04_08_041622) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
