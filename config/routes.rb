efdc36d7b2572a151ce91e631f0774d7fc0fde5a
ekateriniy <ekaterin.yu@gmail.com>
2022-04-13 10:45:07 +0400

    refactor: replace only with except option in route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :tests do
-    resources :questions, shallow: true, only: [:new, :create, :edit, :show, :destroy, :update, :delete]
+    resources :questions, shallow: true, except: :index
   end
 end
