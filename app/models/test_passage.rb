2c94b2ca0750aa779738adacdfa9d27182e451ad
ekateriniy <ekaterin.yu@gmail.com>
2022-04-15 11:34:41 +0400

    refactor: combine test_passage callbacks

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,8 +5,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :next_question
+  before_validation :set_current_question
 
   def completed?
     current_question.nil?
@@ -19,7 +18,7 @@ class TestPassage < ApplicationRecord
   end
 
   def current_question_number
-    test.questions.where('id <= ?', current_question_id).count
+    test.questions.order(:id).where('id < ?', current_question_id).count + 1
   end
 
   def pass?
@@ -28,8 +27,8 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def set_current_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
@@ -41,6 +40,10 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+    if test.present? && current_question.nil?
+      test.questions.first
+    else
+      test.questions.order(:id).where('id > ?', current_question.id).first
+    end
   end
 end
