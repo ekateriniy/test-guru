f785f84a6419f436fa7746a43e4ac1379ef82ed4
ekateriniy <ekaterin.yu@gmail.com>
2022-04-16 19:43:57 +0400

    refactor: add helper for flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to repo.capitalize, "https://github.com/#{author}/#{repo}"
   end
+
+  def alert_flash_message
+    content_tag :p, flash[:alert], class: 'flash alert'
+  end
 end
