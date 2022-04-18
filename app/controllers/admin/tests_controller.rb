b558f6985776665bafd011093a238318833cd0c9
ekateriniy <ekaterin.yu@gmail.com>
2022-04-18 14:31:15 +0400

    feat: add admin

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,60 @@
+class Admin::TestsController < Admin::BaseController
+  
+  before_action :set_test, only: [:show, :update, :edit, :destroy, :start]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+  end
+
+  def create
+    @test = Test.new(test_params)
+    
+    # temporaly adding 1st user from the db to the new test.author
+    @test.author = User.first
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+end
