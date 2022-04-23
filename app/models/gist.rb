3b33385fda8209ddad21a06eafdf064cd15eb9b5
ekateriniy <ekaterin.yu@gmail.com>
2022-04-23 11:08:07 +0400

    refactor: use truncate helper instead of a model method

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -4,10 +4,6 @@ class Gist < ApplicationRecord
 
   validates :url, presence: true
 
-  def shorted_question
-    question.body.length > 25 ? question.body[0..26] : question.body
-  end
-
   def url_hash
     url.split('.com/')[1]
   end
